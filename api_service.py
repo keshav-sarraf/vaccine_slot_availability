9382d9c95761ca735dc30893b12a9683c9d6da62
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-05-03 02:35:36 +0530

    updated getting all districts api

--- api_service.py
+++ api_service.py
@@ -1,20 +1,36 @@
 import json
+import os
 from functools import lru_cache
+from urllib.parse import urljoin
 
 import requests
+
+
+# TODO: fix this
+# ROOT_URL = r"https://cdn-api.co-vin.in/api"
+
+def get_all_state_codes():
+    headers = {'Content-type': 'application/json', 'accept': 'application/json', 'Accept-Language': 'hi_IN'}
+    response = requests.get("https://cdn-api.co-vin.in/api/v2/admin/location/states", headers=headers)
+    json_data = response.json()["states"]
+    return json_data
 
 
 @lru_cache()
 def get_all_dist_codes():
     dist_codes = list()
 
-    for state_code in range(1, 42):
-        print("State code: ", state_code)
-        response = requests.get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/{}".format(state_code))
+    states = get_all_state_codes()
+
+    for state in states:
+        print("State: ", state)
+        response = requests.get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/{}".format(state["state_id"]))
         json_data = json.loads(response.text)
         for dist_info in json_data["districts"]:
             dist_codes.append({"dist_id": dist_info["district_id"],
-                               "dist_name": dist_info["district_name"]}
+                               "dist_name": dist_info["district_name"],
+                               "state_id": state["state_id"],
+                               "state_name": state["state_name"]}
                               )
     return dist_codes
 
@@ -23,13 +39,15 @@ def get_filtered_dists(search_query):
     dist_codes = get_all_dist_codes()
 
     if search_query:
-        filtered_dists = list(filter(lambda x: search_query in x['dist_name'], dist_codes))
+        filtered_dists = list(filter(lambda x: search_query in x['dist_name'].lower()
+                                               or search_query in x['state_name'].lower()
+                                     , dist_codes))
     else:
         filtered_dists = dist_codes
-
-    filtered_dists = list(map(lambda x: x["dist_name"], filtered_dists))
-
     return filtered_dists
 
-#dist_codes = get_all_dist_codes()
-#print(dist_codes)
+
+# dist_codes = get_all_dist_codes()
+# print(dist_codes)
+
+#print(get_all_state_codes())
--- main.py
+++ main.py
@@ -4,22 +4,40 @@ from api_service import get_all_dist_codes, get_filtered_dists
 app = Flask(__name__)
 
 
+@app.route('/district/<dist_name>', methods=['GET'])
+def get_slots(dist_name):
+    return "Hello World" + dist_name
+
+
 @app.route('/districts', methods=['GET'])
 def get_districts():
     query = request.args.get('q')
     filtered_dists = get_filtered_dists(query)
-
-    return jsonify(filtered_dists)
+    print(filtered_dists)
+    filtered_dists_list = list(map(lambda x: "{} | {}".format(x["dist_name"], x["state_name"]), filtered_dists))
+    return jsonify(filtered_dists_list)
 
 
 @app.route('/')
 def hello_world():
     return render_template("landing.html")
 
+
 '''
 for notification:
     email,
     device info ?
+    
+    dist_id:
+        dist_name
+        notifiers:[
+            p1:
+                is_notified?
+                email:
+                push_id:
+        ]
+        
+    
 '''
 
 if __name__ == '__main__':
