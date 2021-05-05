aa3148d314ff5741bfd045fe2899b3349964b768
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-05-05 16:32:46 +0530

    refreshing list of servicable districts once

--- db_refresh.py
+++ db_refresh.py
@@ -38,15 +38,20 @@ def _refresh_slots(dist_id_to_refresh, dist_info_dict):
     _add_slots(dist_id_to_refresh, dist_info_dict)
 
 
-dist_info_list = get_all_dist_codes_api()
-dist_info_list = sorted(dist_info_list, key=lambda x: x["state_name"])
+def _refresh_and_get_dist_info_list():
+    api_dist_info_list = get_all_dist_codes_api()
+    api_dist_info_list = sorted(api_dist_info_list, key=lambda x: x["state_name"])
 
-# for dist_info in dist_info_list:
-#     dist_id = dist_info["dist_id"]
-#
-#     key = str(dist_id)
-#     doc_ref = db.collection(u'static').document(key)
-#     print(doc_ref.set(dist_info, merge=True))
+    # update in firebase
+    for dist_info_itr in api_dist_info_list:
+        key = str(dist_info_itr["dist_id"])
+        doc_ref = db.collection(u'static').document(key)
+        print(doc_ref.set(dist_info_itr, merge=True))
+
+    return api_dist_info_list
+
+
+dist_info_list = _refresh_and_get_dist_info_list()
 
 refreshed_districts = dict()
 while True:
